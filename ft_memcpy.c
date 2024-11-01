6f4aa3c1b7b6b6e9d86db50a0801c76f742167e9
AKL001 <l9udyr@gmail.com>
2024-11-01 20:45:58 +0100

    ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ablabib <ablabib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 16:29:13 by ablabib           #+#    #+#             */
-/*   Updated: 2024/11/01 20:24:48 by ablabib          ###   ########.fr       */
+/*   Updated: 2024/11/01 20:45:19 by ablabib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 
-	if (n == 0)
-		return (dest);
 	if (!dest && !src)
 		return (NULL);
 	i = 0;
