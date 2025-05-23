0452f13f92c6b189460e97912ea4d4d89ba8a199
Abdelkarim Labib <133284045+AKL001@users.noreply.github.com>
2025-05-24 00:07:24 +0100

    ft_calloc.c guard

--- ft_calloc.c
+++ ft_calloc.c
@@ -16,6 +16,9 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
 
+	// Guard against integer overflow in multiplication: nmemb * size
+	// If this overflows, malloc may allocate less memory than expected,
+	// leading to buffer overflows and undefined behavior.
 	if (nmemb > 0 && size > SIZE_MAX / nmemb)
 		return (NULL);
 	if (nmemb == 0 && size == 0)
